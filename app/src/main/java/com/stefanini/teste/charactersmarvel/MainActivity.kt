2ef5743977a37bd5019b4c73bdcd101b3dfaf991
kadusouza <kdusouza@gmail.com>
2020-10-20 08:32:36 -0300

    feat: Implementado dialog de loading ao carregar mais informações da lista (melhorar futuramente)

--- app/src/main/java/com/stefanini/teste/charactersmarvel/MainActivity.kt
+++ app/src/main/java/com/stefanini/teste/charactersmarvel/MainActivity.kt
@@ -1,15 +1,18 @@
 package com.stefanini.teste.charactersmarvel
 
+import android.app.AlertDialog
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.KeyEvent
 import android.view.View
+import android.widget.LinearLayout
 import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.stefanini.teste.charactersmarvel.adapter.CharacterAdapter
 import com.stefanini.teste.charactersmarvel.data.local.CharacterEntity
@@ -43,6 +46,8 @@ class MainActivity : DaggerAppCompatActivity() {
 
     var limit = 50
 
+    lateinit var cDialog: AlertDialog
+
     @Inject
     lateinit var providerFactory: ViewModelProviderFactory
 
@@ -62,15 +67,18 @@ class MainActivity : DaggerAppCompatActivity() {
             when (it.status) {
                 ResponseStatus.LOADING -> {
                     Log.d(TAG, "onCreate: Consultando API")
-                    //pbLoading.visibility = View.VISIBLE
                     txtLoading.text = "Buscando Informações"
                 }
                 ResponseStatus.ERROR -> {
                     pbLoading.visibility = View.GONE
+                    //llProgress.visibility = View.GONE
                     txtLoading.text = it.message
                 }
                 ResponseStatus.SUCCESS -> {
                     pbLoading.visibility = View.GONE
+                    if (loading) {
+                        cDialog.dismiss()
+                    }
                     txtLoading.visibility = View.GONE
                     it.data?.forEach { it ->
                         val formatOrigin = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss-SSSS")
@@ -139,9 +147,25 @@ class MainActivity : DaggerAppCompatActivity() {
                 if (!loading && lastVisibleItem == totalItem - 1) {
                     loading = true
                     offset += limit
+                    progress()
                     mainViewModel.getCharacters(offset, limit)
                 }
             }
         })
+    }
+
+    fun progress() {
+        cDialog = AlertDialog.Builder(this)
+            .setView(layoutInflater.inflate(R.layout.dialog_loading, null))
+            .setCancelable(false)
+            .setOnKeyListener { dialog, keyCode, event ->
+                if (keyCode == KeyEvent.KEYCODE_POWER) {
+                    val closeDialog = Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS)
+                    sendBroadcast(closeDialog)
+                }
+                false
+            }
+            .create()
+        cDialog.show()
     }
 }
